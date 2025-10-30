f14a27374ecf78cdab2a538827360db010af3732
Mobile User <1107154510@qq.com>
2025-10-30 21:49:39 +0800

    update of from AD 添加 16.5 和 15.4的 iOS SDK 2025-10-30 21:49

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ cd /var/mobile/Github/iOS-SDKs
 
 git add .
 git commit -m "update of from AD 添加 16.5 和 15.4的 iOS SDK $(date +'%Y-%m-%d %H:%M')"
-git push origin main
+git push origin master
 gh pr create
